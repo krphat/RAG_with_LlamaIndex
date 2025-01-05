207206cd3704fa7729f329ea7f6778a48b1c9847
Phat <keiryphat@gmail.com>
2025-01-05 15:21:02 +0700

    config CORS

--- APIs/app.py
+++ APIs/app.py
@@ -1,18 +1,35 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routes.upload_document import router as upload_router
 from routes.conversations import router as conversations_router
 
-version = "v1"
+version = "v2"
+# Danh sách các origin được phép
+origins = [
+    "http://localhost:3001",  # Origin của frontend
+    "http://127.0.0.1:3001", # Nếu cần thêm domain
+]
+
+
 
 app = FastAPI(
     title="RAG",
     description='A REST API for Chatbot supports learning English.',
     version=version,
-    contact={ 
+    contact={
         "email": "keiryphat@gmail.com"
     },
     docs_url=f"/api/{version}/docs"
+)
+
+# Thêm middleware xử lý CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Các domain được phép
+    allow_credentials=True,  # Có cho phép gửi cookies hay không
+    allow_methods=["*"],  # Các HTTP method được phép
+    allow_headers=["*"],  # Các HTTP header được phép
 )
 
 # Include routers
@@ -25,5 +42,5 @@ if __name__ == "__main__":
 
 """
     Run server: python app.py
-    APIs document: http://127.0.0.1:8000/api/v1/docs
+    APIs document: http://127.0.0.1:8000/api/v2/docs
 """
--- README.md
+++ README.md
@@ -65,7 +65,7 @@ cd APIs
 ```bash
 python app.py
 ```
-View APIs document at: `http://127.0.0.1:8000/api/v1/docs`
+View APIs document at: `http://127.0.0.1:8000/api/v2/docs`
 
 
 
